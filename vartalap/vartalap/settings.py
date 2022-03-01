82c52b8180bdd7195389ba34fc583dc443588c6a
SUSHIL <sushil@SUSHILs-MacBook-Pro.local>
2022-03-01 15:51:56 +0530

    settings updated

--- vartalap/vartalap/settings.py
+++ vartalap/vartalap/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os 
 
 from pathlib import Path
 
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
